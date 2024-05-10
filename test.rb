f5571cc0ecb4415e0abdfb0c2d14acab2b28c154
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-10 18:28:45 +0000

    first page working

--- test.rb
+++ test.rb
@@ -1,10 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
 require "http"
 require "json"
-require "better_errors"
 
-# parsing the API itself
 api_text = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
 
 raw_api=HTTP.get(api_text)
@@ -12,16 +8,18 @@ api_parsed = JSON.parse(raw_api)
 
 # go digging around for the 3 letter country codes 
 api_curr = api_parsed.fetch("currencies") # this is the has that has the 3 letter codes linked to names
+api_3lcountry = api_curr.keys # this is an array we can loop over!!
 
-
-# the actual webpage
-get("/") do
-  @api_3lcountry = api_curr.keys # this is an array we can loop over!!
-
-  erb(:home)
-end
-
-get("/:country") do 
-  @original_currency = params.fetch("country")
-  erb(:country)
+api_3lcountry.each do |country|
+  pp country
 end 
+
+#api_quotes = api_parsed.fetch("quotes")
+
+# first get the list of 
+#pp country
+<ul>
+<% @api_3lcountry.each do |country| %>
+  <li> country </li>
+<end %>
+</ul>
