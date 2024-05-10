aa6d7fb16af5348d99dda776c366415985b7e848
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-10 19:44:16 +0000

    done

--- app.rb
+++ app.rb
@@ -4,18 +4,17 @@ require "http"
 require "json"
 require "better_errors"
 
-# parsing the API itself
-api_text = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
-
-raw_api=HTTP.get(api_text)
-api_parsed = JSON.parse(raw_api)
-
-# go digging around for the 3 letter country codes 
-api_curr = api_parsed.fetch("currencies") # this is the has that has the 3 letter codes linked to names
-
 
 # the actual webpage
 get("/") do
+  # parsing the API itself
+  api_text = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
+
+  raw_api=HTTP.get(api_text)
+  api_parsed = JSON.parse(raw_api)
+
+  # go digging around for the 3 letter country codes 
+  api_curr = api_parsed.fetch("currencies") # this is the has that has the 3 letter codes linked to names
   @api_3lcountry = api_curr.keys # this is an array we can loop over!!
 
   erb(:home)
@@ -23,5 +22,31 @@ end
 
 get("/:country") do 
   @original_currency = params.fetch("country")
+
+  api_text = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
+
+  raw_api=HTTP.get(api_text)
+  api_parsed = JSON.parse(raw_api)
+
+  # go digging around for the 3 letter country codes 
+  api_curr = api_parsed.fetch("currencies") # this is the has that has the 3 letter codes linked to names
+  @api_3lcountry = api_curr.keys # this is an array we can loop over!!
+
   erb(:country)
+end 
+
+get("/:country/:conversion") do 
+  @original_currency = params.fetch("country")
+  @conversion_currency = params.fetch("conversion")
+
+  total = @original_currency + @conversion_currency
+
+  # get the api 
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_KEY")}&from=#{@original_currency}&to=#{@conversion_currency}&amount=1"
+
+  raw_api_info=HTTP.get(api_url)
+  api_parsed_info = JSON.parse(raw_api_info)
+  @quote_val = api_parsed_info.fetch("result")
+
+  erb(:conversion)
 end 
--- test.rb
+++ test.rb
@@ -2,24 +2,11 @@ require "http"
 require "json"
 
 api_text = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
+api_info = "https://api.exchangerate.host/live?access_key=" + ENV.fetch("EXCHANGE_KEY") + "&source=AUD" # first get it for AUD
 
-raw_api=HTTP.get(api_text)
+raw_api=HTTP.get(api_info)
 api_parsed = JSON.parse(raw_api)
+quote_hash = api_parsed.fetch("quotes") # this is where you go digging for the values 
 
-# go digging around for the 3 letter country codes 
-api_curr = api_parsed.fetch("currencies") # this is the has that has the 3 letter codes linked to names
-api_3lcountry = api_curr.keys # this is an array we can loop over!!
-
-api_3lcountry.each do |country|
-  pp country
-end 
-
-#api_quotes = api_parsed.fetch("quotes")
-
-# first get the list of 
-#pp country
-<ul>
-<% @api_3lcountry.each do |country| %>
-  <li> country </li>
-<end %>
-</ul>
+quote_val = quote_hash.fetch("AUDAED")
+pp quote_val
